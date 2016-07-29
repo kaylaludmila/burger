9d5eaefb332dea65bd8ab067c92fe0c72b550723
Kayla Martin <kaylaludmila@gmail.com>
2016-07-28 23:46:01 -0500

    attempt to add Jaws

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,24 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burger_db'
-});
+
+var source = {
+
+    localhost: {
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burger_db'
+    },
+    jawsDB: {
+        port: 3306,
+        host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'yq8ffbegvy22igma',
+        password: "o3yh4aklr0ynr9zz",
+        database: "gdrso0xua343sf1k"
+    }
+}
+
+var connection = mysql.createConnection(source.jawsDB);
 
 
 connection.connect(function(err) {
